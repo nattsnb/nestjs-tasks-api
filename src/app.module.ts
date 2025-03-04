96a71d80628d5861df6c5e8f775fb47455e1bac4
Natasza ASzatan <natiye.nb@gmai.com>
2025-03-04 19:37:23 +0100

    aded database dir. it contains prisma service and databas module. added database table articles, provided rest api functionality using prismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from 'joi';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
+    TasksModule,
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         DATABASE_URL: Joi.string().required(),
