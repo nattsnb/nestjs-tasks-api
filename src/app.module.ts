f207b286965821615f73e215327f98c79d5ea81f
Natasza ASzatan <natiye.nb@gmai.com>
2025-03-04 18:39:26 +0100

    added docker env and compose files, added and inited prisma. now the database can be accesseed through posstgres and pgadmin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
